9801f8eb25e2fb5b2e380a856753751e10bf5d5b
adisak1618 <adisakchaiyakul@gmail.com>
2023-10-31 01:29:25 +0700

    test loading at root

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <main className="w-full h-screen bg-[white] flex items-center justify-center text-center">
+      <div>
+        <h2 className="text-h1 font-bold text-third">Loading...</h2>
+        <p className="text-h5 text-text-secondary">
+          Getting Everything ready for you soon
+        </p>
+      </div>
+    </main>
+  );
+}
