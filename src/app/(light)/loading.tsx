9e29646727e0f5b4b16613e19c3d1693ac1f4be4
adisak1618 <adisakchaiyakul@gmail.com>
2023-10-31 01:43:13 +0700

    improve ui

--- src/app/(light)/loading.tsx
+++ src/app/(light)/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <main className="w-full h-screen bg-[white] flex items-center justify-center text-center">
       <div>
-        <h2 className="text-h1 font-bold text-third">Loading...</h2>
+        <h2 className="text-h2 md:text-h1 font-bold text-third">Loading...</h2>
         <p className="text-h5 text-text-secondary">
           Getting Everything ready for you soon
         </p>
