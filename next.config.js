dcd5e0760b5de54e4e7e22fe9ff74643a674cc1f
adisak1618 <adisakchaiyakul@gmail.com>
2023-10-31 15:17:37 +0700

    remove log on prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
+  },
   images: {
     domains: ["secure.gravatar.com", "2.gravatar.com", "klabban-demo.local", "purple-gazelle-147770.hostingersite.com"]
   }
