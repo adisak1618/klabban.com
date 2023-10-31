d7fedbd48cb9376962fa76be0ea3f46268a27c92
adisak1618 <adisakchaiyakul@gmail.com>
2023-10-31 14:39:14 +0700

    add more domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["2.gravatar.com", "klabban-demo.local", "purple-gazelle-147770.hostingersite.com"]
+    domains: ["secure.gravatar.com", "2.gravatar.com", "klabban-demo.local", "purple-gazelle-147770.hostingersite.com"]
   }
 }
 
