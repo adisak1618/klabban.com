deb0561415fc17f6372d34b4540f0a2d7627c382
adisak1618 <adisakchaiyakul@gmail.com>
2023-10-31 15:48:54 +0700

    basic seo for post

--- src/app/(light)/blog/[blogSlug]/page.tsx
+++ src/app/(light)/blog/[blogSlug]/page.tsx
@@ -1,9 +1,5 @@
 import { GutenbergContent } from "klabban-commerce/react";
-import {
-  PostFragmentFragment,
-  PostProvider,
-  PostRequest,
-} from "klabban-commerce";
+import { PostFragmentFragment, PostProvider } from "klabban-commerce";
 import { KlabbanConfig } from "libs/klabbanConfig";
 import { format } from "date-fns";
 // import "klabban-commerce/react/index.css";
@@ -14,14 +10,24 @@ import { PostTags } from "./tags";
 import { HeadlineSection } from "components/Headline";
 import { AuthorCard } from "./author";
 import { Breadcrumb } from "components/Breadcrumb";
-import { MainMenu } from "components/MainMenu";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
+import { siteName } from "config/siteConfig";
 
 export interface BlogDetailPageProps {
   params: {
     blogSlug: string;
   };
+}
+
+async function fetchPost(slug: string) {
+  return await PostProvider({
+    ...KlabbanConfig,
+    slug: slug,
+    queryOption: {
+      includeNextPreviousPost: true,
+      includeTags: true,
+    },
+  });
 }
 
 const findRootCategory = (categories: PostFragmentFragment["categories"]) => {
@@ -38,15 +44,25 @@ const findRootCategory = (categories: PostFragmentFragment["categories"]) => {
   return [];
 };
 
+export async function generateMetadata({ params }: BlogDetailPageProps) {
+  const { data: post } = await fetchPost(params.blogSlug);
+  return {
+    title: post?.title,
+    description: post?.excerpt?.replaceAll(
+      /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g,
+      ""
+    ),
+    siteName,
+    url: `${process.env.BASE_URL}/blog/${post?.slug}`,
+    images: [post?.featuredImage?.node.medium],
+    locale: "th_TH",
+    type: "website",
+  };
+}
+
 export default async function BlogDetailPage({ params }: BlogDetailPageProps) {
-  const { Provider, data: post } = await PostProvider({
-    ...KlabbanConfig,
-    slug: params.blogSlug,
-    queryOption: {
-      includeNextPreviousPost: true,
-      includeTags: true,
-    },
-  });
+  const { Provider, data: post } = await fetchPost(params.blogSlug);
+
   if (!post) return notFound();
   return (
     <>
