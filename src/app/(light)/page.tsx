8a21b494145aaecefe570a64f838c6b2863127b6
adisak1618 <adisakchaiyakul@gmail.com>
2023-11-06 00:33:29 +0700

    demo homepage

--- src/app/(light)/page.tsx
+++ src/app/(light)/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { useViewer } from "klabban-commerce/react";
-import { useSession, signIn, signOut } from "klabban-commerce/auth";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import { Button } from "ui/button";
@@ -11,33 +9,58 @@ export default function Home() {
     target: ref,
     offset: ["start start", "end start"],
   });
-  const { data, status } = useSession();
-  const [{ data: viewer }] = useViewer({});
-
-  if (!data) {
-    return <button onClick={() => signIn()}>login</button>;
-  }
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "20%"]);
+  const textX = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
+  const backgroundTextY = useTransform(scrollYProgress, [0, 1], ["0%", "-30%"]);
 
   return (
-    <div>
-      <div className="page-hero-shape h-[98vh] w-full overflow-hidden gray-gradient relative">
-        <div className="leading-[200px] mt-20 lg:mt-10 lg:leading-[250px] text-white text-[150px] lg:text-[250px] opacity-30 font-bold absolute w-full h-full left-0 top-0 text-center">
+    <div className="">
+      <div className="relative page-hero-shape h-[98vh] w-full overflow-hidden gray-gradient relative">
+        <motion.div
+          style={{
+            y: backgroundTextY,
+          }}
+          className="leading-[200px] mt-20 lg:mt-10 lg:leading-[250px] text-white text-[150px] lg:text-[250px] opacity-30 font-bold absolute w-full h-full left-0 top-0 text-center"
+        >
           <p className=" animate-marquee whitespace-nowrap">
             WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
           </p>
           <p className=" animate-marquee2 whitespace-nowrap">
             WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
           </p>
-        </div>
+          <p className=" animate-marquee whitespace-nowrap">
+            WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
+          </p>
+          <p className=" animate-marquee2 whitespace-nowrap">
+            WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
+          </p>
+        </motion.div>
         {/* <div className="hidden lg:flex text-[250px] animate-marquee lg:animate-none text-white opacity-30 font-bold absolute w-full h-1/2 justify-center items-center">
             <p className="">WELCOME</p>
           </div> */}
-        <div className="container max-w-6xl w-full h-full relative mx-auto">
-          <motion.img
-            src="/images/hero.webp"
-            className="absolute bottom-8 lg:bottom-0 lg:right-0 left-1/2 lg:left-auto ml-[-400px] lg:ml-0 min-w-[800px] object-cover"
-          ></motion.img>
-          <div className="absolute flex flex-col justify-center items-center bottom-0 mb-28 lg:mb-40 w-full lg:max-w-lg px-10 left-0">
+
+        <div className="container relative max-w-6xl w-full h-full  mx-auto">
+          <motion.div
+            className="w-full h-full absolute bottom-8 lg:bottom-0 lg:right-0 left-1/2 lg:left-auto ml-[-400px] lg:ml-0 min-w-[800px]"
+            style={{
+              y: backgroundY,
+            }}
+          >
+            <img
+              alt="hero"
+              ref={ref}
+              src="/images/hero.webp"
+              className="absolute bottom-8 right-0"
+            />
+          </motion.div>
+
+          <motion.div
+            // style={{
+            //   x: textX,
+            //   y: 0,
+            // }}
+            className="absolute flex flex-col justify-center items-center bottom-0 mb-28 lg:mb-40 w-full lg:max-w-lg px-10 left-0"
+          >
             <h1 className="text-h2 leading-[1em] text-center sm:text-h1 font-title font-bold text-white uppercase">
               Im Soryor.
             </h1>
@@ -54,15 +77,10 @@ export default function Home() {
                 About Me
               </Button>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="h-[300vh]"></div>
-      <p>Hello, {viewer?.viewer?.email}</p>
-      <p>status: {status}</p>
-      {/* <p>{JSON.stringify(data)}</p> */}
-
-      <button onClick={() => signOut()}>logout</button>
     </div>
   );
 }
