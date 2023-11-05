06925eb225c23c4c9354045be34a82e00979eb91
adisak1618 <adisakchaiyakul@gmail.com>
2023-11-05 23:29:37 +0700

    add parallax hero

--- src/app/(light)/page.tsx
+++ src/app/(light)/page.tsx
@@ -3,6 +3,7 @@ import { useViewer } from "klabban-commerce/react";
 import { useSession, signIn, signOut } from "klabban-commerce/auth";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import { Button } from "ui/button";
 
 export default function Home() {
   const ref = useRef(null);
@@ -20,20 +21,39 @@ export default function Home() {
   return (
     <div>
       <div className="page-hero-shape h-[98vh] w-full overflow-hidden gray-gradient relative">
+        <div className="leading-[200px] mt-20 lg:mt-10 lg:leading-[250px] text-white text-[150px] lg:text-[250px] opacity-30 font-bold absolute w-full h-full left-0 top-0 text-center">
+          <p className=" animate-marquee whitespace-nowrap">
+            WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
+          </p>
+          <p className=" animate-marquee2 whitespace-nowrap">
+            WELCOME WELCOME WELCOME WELCOME WELCOME WELCOME
+          </p>
+        </div>
+        {/* <div className="hidden lg:flex text-[250px] animate-marquee lg:animate-none text-white opacity-30 font-bold absolute w-full h-1/2 justify-center items-center">
+            <p className="">WELCOME</p>
+          </div> */}
         <div className="container max-w-6xl w-full h-full relative mx-auto">
           <motion.img
             src="/images/hero.webp"
-            className="absolute bottom-0 right-0"
+            className="absolute bottom-8 lg:bottom-0 lg:right-0 left-1/2 lg:left-auto ml-[-400px] lg:ml-0 min-w-[800px] object-cover"
           ></motion.img>
-          <div className="absolute bottom-1/2 w-1/2 left-0">
-            <h1 className="text-h1 font-title font-bold text-white uppercase">
+          <div className="absolute flex flex-col justify-center items-center bottom-0 mb-28 lg:mb-40 w-full lg:max-w-lg px-10 left-0">
+            <h1 className="text-h2 leading-[1em] text-center sm:text-h1 font-title font-bold text-white uppercase">
               Im Soryor.
             </h1>
-            <p className="text-h6 font-title font-medium text-white">
+            <p className="text-h6 font-title font-medium text-white text-center lg:text-left">
               I create travel guides and backpacking itineraries for countries
               such as Thailand and New Zealand, in addition to sharing
               photography resources and more.
             </p>
+            <div className="flex gap-4 mt-4">
+              <Button size="xl" className="rounded-full">
+                Read My Blog
+              </Button>
+              <Button variant="secondary" size="xl" className="rounded-full">
+                About Me
+              </Button>
+            </div>
           </div>
         </div>
       </div>
---
+++ tailwind.config.js
@@ -0,0 +1,114 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontSize: {
+        h1: "80px",
+        h2: "60px",
+        h3: "40px",
+        h4: "30px",
+        h5: "24px",
+        h6: "20px",
+        body: "16px",
+        caption: "14px",
+        small: "12px",
+        tiny: "10px",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        // old
+        "transparent": "transparent",
+        "primary": "var(--primary-color)",
+        "secondary": "var(--bg-secondary)",
+        "light": "var(--text-light)",
+        "third": "var(--bg-third)",
+        "light-gray": "var(--light-gray)",
+        "lighter-gray": "var(--lighter-gray)",
+        "text-color": "var(--text-color)",
+        "text-secondary": "var(--text-secondary)",
+        "text-third": "var(--text-third)",
+        "text-hover": "var(--text-hover)",
+        "white": "#ffffff",
+        "border": "var(--slim-border)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "marquee": {
+          '0%': { transform: 'translateX(-50%)' },
+          '50%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        "marquee2": {
+          '0%': { transform: 'translateX(-50%)' },
+          '50%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "marquee": 'marquee 25s linear infinite',
+        "marquee2": 'marquee2 25s linear infinite',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
