f28e49ce1c1cfe273005e9e9b69db8eb7166d527
adisak1618 <adisakchaiyakul@gmail.com>
2023-11-10 08:50:07 +0700

    add header

--- src/container/pageDetail/content.tsx
+++ src/container/pageDetail/content.tsx
@@ -23,49 +23,49 @@ export function PageContent({
       <MainMenu
         light={pageCustomUI?.customPageUI?.mainContent?.lightNavigation}
       />
-      {/* {pageCustomUI?.customPageUI?.mainContent?.enable && (
-        <div
-          style={{
-            order: pageCustomUI?.customPageUI?.mainContent?.order || 99,
-          }}
-        >
-          {pageCustomUI?.customPageUI?.mainContent?.headerStyle !== "simple" &&
-            pageCustomUI?.customPageUI?.mainContent?.showHeader && (
-              <HeadlineSection
-                className="h-[80vh] md:h-[40vh] !bg-center"
-                backgroundImage={page?.featuredImage?.node.sourceUrl || ""}
-                backgroundSrcSet={page?.featuredImage?.node.srcSet}
-                imageAlt={page?.featuredImage?.node.altText}
-                title={page?.title || ""}
-                hideSubTitle
-              />
-            )}
-          {pageCustomUI?.customPageUI?.mainContent?.headerStyle === "simple" &&
-            pageCustomUI.customPageUI.mainContent.showHeader && (
-              <div className="container-content pt-6">
-                <h1 className="text-h3 font-bold text-center">{page?.title}</h1>
-              </div>
-            )}
-          {pageCustomUI?.customPageUI?.mainContent?.showBreadcrumbs && (
-            <div className="mx-auto !max-w-5xl mt-6 mb-4 lg:container px-5">
-              <Breadcrumb
-                links={[
-                  {
-                    label: page?.title || "",
-                    href: `/${page?.databaseId}`,
-                  },
-                ]}
-              />
+
+      <div
+        style={{
+          order: pageCustomUI?.customPageUI?.mainContent?.order || 99,
+        }}
+      >
+        {pageCustomUI?.customPageUI?.mainContent?.headerStyle !== "simple" &&
+          pageCustomUI?.customPageUI?.mainContent?.showHeader && (
+            <HeadlineSection
+              className="h-[80vh] md:h-[40vh] !bg-center"
+              backgroundImage={page?.featuredImage?.node.sourceUrl || ""}
+              backgroundSrcSet={page?.featuredImage?.node.srcSet}
+              imageAlt={page?.featuredImage?.node.altText}
+              title={page?.title || ""}
+              hideSubTitle
+            />
+          )}
+        {pageCustomUI?.customPageUI?.mainContent?.headerStyle === "simple" &&
+          pageCustomUI.customPageUI.mainContent.showHeader && (
+            <div className="container-content pt-6">
+              <h1 className="text-h3 font-bold text-center">{page?.title}</h1>
             </div>
           )}
+        {/* {pageCustomUI?.customPageUI?.mainContent?.showBreadcrumbs && (
+          <div className="mx-auto !max-w-5xl mt-6 mb-4 lg:container px-5">
+            <Breadcrumb
+              links={[
+                {
+                  label: page?.title || "",
+                  href: `/${page?.databaseId}`,
+                },
+              ]}
+            />
+          </div>
+        )} */}
 
-          <GutenbergContent
-            // entryClassName="!my-0"
-            // className="!my-0"
-            content={page?.content || ""}
-          />
-        </div>
-      )} */}
+        <GutenbergContent
+          // entryClassName="!my-0"
+          // className="!my-0"
+          content={page?.content || ""}
+        />
+      </div>
+
       <div>
         <div className="flex flex-col">
           {pageCustomUI?.customPageUI?.parallax?.enable && (
