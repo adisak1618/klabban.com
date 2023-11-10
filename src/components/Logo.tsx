f4e675a793d4216ba549e248523992f63ce6ec6b
adisak1618 <adisakchaiyakul@gmail.com>
2023-11-10 16:23:49 +0700

    remove static website name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,13 +4,7 @@ export function Logo({ logo }: { logo?: string }) {
   return (
     <div className="h-[40px] relative flex items-center gap-1">
       {logo ? (
-        <Image
-          className="pb-3"
-          width={80}
-          height={80}
-          src={logo || ""}
-          alt={"site Logo"}
-        />
+        <Image width={80} height={80} src={logo || ""} alt={"site Logo"} />
       ) : (
         <img
           className="w-full h-full text-white"
@@ -18,7 +12,6 @@ export function Logo({ logo }: { logo?: string }) {
           alt="site Logo"
         />
       )}
-      <span className="font-bold text-h5 ">KLAB BAN</span>
     </div>
   );
 }
