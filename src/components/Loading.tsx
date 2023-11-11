b1c91922fb8800a30777458680d476c916936519
adisak1618 <adisakchaiyakul@gmail.com>
2023-11-11 08:21:36 +0700

    add white bg

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function LoadingBox() {
   return (
-    <main className="w-full h-screen bg-[white] flex items-center justify-center text-center p-6">
+    <main className="w-full h-screen bg-white flex items-center justify-center text-center p-6">
       <div>
         <h2 className="text-h2 md:text-h1 font-bold text-text-secondary">
           Loading...
